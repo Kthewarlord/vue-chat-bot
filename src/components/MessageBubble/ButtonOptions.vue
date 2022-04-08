086e1e19744c9d7a8b4c3196a55262ddcf148e2a
Kthewarlord <i.amken123@hotmail.com>
2022-04-08 14:51:59 +0700

    Update ButtonOptions to accept new line \n

--- src/components/MessageBubble/ButtonOptions.vue
+++ src/components/MessageBubble/ButtonOptions.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="qkb-msg-bubble-component qkb-msg-bubble-component--button-options">
     <div class="qkb-msg-bubble-component__text">
-      <span>{{ mainData.text }}</span>
+      <span
+        v-for="(line,lineNumber) of mainData.text.split('\n')" 
+        v-bind:key="lineNumber" >
+        {{ line }}<br/>
+      </span>
     </div>
     <div class="qkb-msg-bubble-component__options-wrapper">
       <div class="qkb-mb-button-options__item" v-for="(item, index) in mainData.options" :class="{ active: selectedItem === item.value }" :key="index">
